9a79922b1d44d577230c03f901a558f88dbabab3
jakubserwin <kuba.serwin99@gmail.com>
2023-02-22 15:37:32 +0100

    configure tailwind with custom font family and colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,28 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Rubik', ...defaultTheme.fontFamily.sans],
+      },
+    },
+    // colors: {
+    //   custom: {
+    //     50: '#fdf8f6',
+    //     100: '#f2e8e5',
+    //     200: '#eaddd7',
+    //     300: '#e0cec7',
+    //     400: '#d2bab0',
+    //     500: '#bfa094',
+    //     600: '#a18072',
+    //     700: '#977669',
+    //     800: '#846358',
+    //     900: '#43302b',
+    //   },
+    // },
   },
   plugins: [],
-};
+}
