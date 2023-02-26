a93adcfdc2769f431066e623614b36a556931793
jakubserwin <kuba.serwin99@gmail.com>
2023-02-26 18:05:34 +0100

    add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // https://astro.build/config
-import tailwind from '@astrojs/tailwind'
-import { defineConfig } from 'astro/config'
-import astroI18next from 'astro-i18next'
+// https://astro.build/config
+import netlify from '@astrojs/netlify/functions';
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
+import astroI18next from 'astro-i18next';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), astroI18next()],
-})
+  output: 'server',
+  adapter: netlify(),
+});
