560f9767893d819f1cee3f64a0249f748b1947a4
jakubserwin <kuba.serwin99@gmail.com>
2023-02-27 07:13:20 +0100

    clean up

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
-// https://astro.build/config
-// https://astro.build/config
 import netlify from '@astrojs/netlify/functions';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 import astroI18next from 'astro-i18next';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), astroI18next()],
   output: 'server',
