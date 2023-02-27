fbd4f6d1aa334a612a5b10a57278220fc09b263a
jakubserwin <kuba.serwin99@gmail.com>
2023-02-27 17:12:45 +0100

    vercel test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-import netlify from '@astrojs/netlify/functions';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
 import astroI18next from 'astro-i18next';
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), astroI18next()],
   output: 'server',
-  adapter: netlify(),
+  adapter: vercel(),
 });
