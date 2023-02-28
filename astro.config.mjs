448a496bfcbdb0e14dd1a9543775c7473d15a253
jakubserwin <kuba.serwin99@gmail.com>
2023-02-28 08:34:31 +0100

    clean up files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
-import astroI18next from 'astro-i18next';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroI18next()],
-  output: 'server',
-  adapter: vercel(),
+  integrations: [tailwind()],
 });
